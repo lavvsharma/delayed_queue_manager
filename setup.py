bd3b2436b97e115b35085bc6f21e1ffa5c5613e0
Lav Sharma <lavsharma2016@gmail.com>
2024-06-07 20:45:12 +0530

    🚩 dependency updated

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ package_name = "delayed_queue_manager"
 
 setup(
     name=package_name,
-    version="1.1.0",
+    version="1.1.1",
     packages=find_packages(),
     package_data={
         package_name: [
@@ -16,7 +16,6 @@ setup(
     },
     install_requires=[
         "Flask>=3.0.3",
-        "Flask-SQLAlchemy>=3.1.1",
         "flask-socketio>=5.3.6",
         "configparser>=7.0.0",
         "loguru>=0.7.2"
