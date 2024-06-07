7ad14781dcc4ce6f1b02ce04b31fff78f52d06a4
Lav Sharma <lavsharma2016@gmail.com>
2024-06-07 20:33:38 +0530

    🔖  version incremented

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ package_name = "delayed_queue_manager"
 
 setup(
     name=package_name,
-    version="1.0.0",
+    version="1.1.0",
     packages=find_packages(),
     package_data={
         package_name: [
